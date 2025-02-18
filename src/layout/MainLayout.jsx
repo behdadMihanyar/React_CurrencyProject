a3bc92936931ee375c4b079c83c052b3d2e86998
behdad <bmihanyar7@gmail.com>
2025-02-19 02:17:00 +0330

    updated

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,10 @@
 import Navbar from "../Pages/Navbar";
 import { Outlet } from "react-router-dom";
-import Footer from "../Pages/Footer.jsx";
 const MainLayout = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
-      <Footer />
     </div>
   );
 };
