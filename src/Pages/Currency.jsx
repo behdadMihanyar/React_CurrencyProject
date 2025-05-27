7ba0bb728f6f5aab3f351024860235b6306ca466
behdad <bmihanyar7@gmail.com>
2025-05-27 23:30:42 +0330

    UPDATE

--- src/Pages/Currency.jsx
+++ src/Pages/Currency.jsx
@@ -32,7 +32,7 @@ const Currency = () => {
         <input
           type="text"
           placeholder="جست و جو ..."
-          className=" border mt-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className=" border mt-5 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
           onChange={(e) => setFilter(e.target.value)}
         />
       </div>
